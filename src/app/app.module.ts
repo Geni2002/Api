cce0f3b517502aaa32ab159f45f2927defd0df87
Geni Vanessa <hernandezmargenivanessa@gmail.com>
2022-11-19 14:50:51 -0600

    avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    DetailComponent,
+    PokeTableComponent
   ],
   imports: [
     BrowserModule,
